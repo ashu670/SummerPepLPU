14a89d5a73aaab51b8e4325bf70a869bfb574195
ashu670 <abhaylal122@gmail.com>
2026-07-06 14:38:56 +0530

    new func

---
+++ DSA/LL/01.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        next = nullptr;
+    }
+};
+
+void print(Node* head) {
+    while (head) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+void delEnd(Node*& head) {
+    if (!head) return;
+
+    if (!head->next) {
+        delete head;
+        head = nullptr;
+        return;
+    }
+
+    Node* curr = head;
+
+    while (curr->next->next) {
+        curr = curr->next;
+    }
+
+    delete curr->next;
+    curr->next = nullptr;
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    head->next->next->next->next->next = new Node(6);
+    head->next->next->next->next->next->next = new Node(7);
+    head->next->next->next->next->next->next->next = new Node(8);
+
+    delEnd(head);
+    print(head);
+
+    return 0;
+}
